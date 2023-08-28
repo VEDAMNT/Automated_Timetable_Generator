d6ec877774139ba194408012e30a6f11712c5a6a
VEDAMNT <99802920+VEDAMNT@users.noreply.github.com>
2023-08-28 20:29:51 +0530

    Create time4.py

---
+++ time4.py
@@ -0,0 +1,65 @@
+#------------------- List of days and time slots------------------------#
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+time_slots = ['09:00 - 10:00', '10:00 - 11:00', '11:15 - 12:15', '12:15 - 1:15', '1:45 - 2:45', '2:45 - 3:45']
+
+def A(prompt):
+    try:
+        return input(prompt).strip()
+    except KeyboardInterrupt:
+        print("\nProgram terminated by the user.")
+        exit()
+
+#-----------------------------TEACHERS---------------------------------#
+
+def TEACH(num_teachers):
+    teachers = []
+    for i in range(1, num_teachers + 1):
+        teacher_name = A(f"Enter the name of teacher {i}: ")
+        teachers.append(teacher_name)
+    return teachers
+
+#----------------------------SUBJECTS----------------------------------#
+
+def SUB(num_subjects):
+    subjects = []
+    for i in range(1, num_subjects + 1):
+        subject_name = A(f"Enter the name of subject {i}: ")
+        subjects.append(subject_name)
+    return subjects
+
+def GT(teachers, subjects):
+    timetable = {}
+    for day in days:
+        timetable[day] = {}
+        for time_slot in time_slots:
+            teacher = teachers.pop(0)
+            subject = subjects.pop(0)
+            timetable[day][time_slot] = {'Teacher': teacher, 'Subject': subject}
+            teachers.append(teacher)
+            subjects.append(subject)
+    return timetable
+
+#----------------NUMBER OF CLASSES------------------#
+
+def main():
+    num_classes = int(A("Enter the number of classes: "))
+    num_teachers = int(A("Enter the number of teachers: "))
+    num_subjects = int(A("Enter the number of subjects: "))
+    
+    for _ in range(num_classes):
+        class_name = A("Enter the name of class: ")
+        teachers = TEACH(num_teachers)
+        subjects = SUB(num_subjects)
+        timetable = GT(teachers, subjects)
+        
+        print("\nTimetable for", class_name)
+        for day in days:
+            print(day)
+            for time_slot in time_slots:
+                entry = timetable[day][time_slot]
+                print(f"{time_slot} : {entry['Teacher']} - {entry['Subject']}")
+            print()
+
+if __name__ == "__main__":
+    main()
